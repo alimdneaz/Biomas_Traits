defc061c01b3910f2807f2c37b9c7666c12e2a41
Ali Neaz <neazali77@gmail.com>
2024-08-21 23:44:20 +0600

    add GCA and SCA normal codes

---
+++ GCA_SCA.R
@@ -0,0 +1,52 @@
+library(devtools)
+breedR_repo <- "https://famuvie.github.io/breedR/"
+devtools::install_github("famuvie/breedR", repos = breedR_repo)
+
+library(breedR)
+library(ggplot2)
+set.seed(123)
+
+## Simulation parameters
+n.parents <- c(male = 15L,
+               female = 10L)
+n.matings <- 100
+n.replicates <- 40
+mu = 10        # Intercept
+sigma2_g <- 6  # Genetic variance of the base population
+sigma2_s <- 1  # Variance of the SCA
+sigma2_e <- 1  # Residual variance
+## Generate all crosses and sample a subset
+parents.codes <- list(male = seq.int(n.parents['male']),
+                      female = n.parents['male'] + seq.int(n.parents['female']))
+matings <- expand.grid(parents.codes)
+matings <- matings[sample(prod(n.parents), n.matings),]
+rownames(matings) <- with(matings, paste(male, female, sep = 'x'))
+
+## Simulated values
+GCA = sapply(do.call('c', parents.codes),
+             function(x) rnorm(1, mean = 0, sd = sqrt(sigma2_g)))
+SCA = sapply(rownames(matings),
+             function(x) rnorm(1, mean = 0, sd = sqrt(sigma2_s)))
+
+## Expected phenotype per family
+eta.family <- mu + SCA + (GCA[matings$male] + GCA[matings$female])/2
+
+## Realised Breeding Values in the progeny
+## (intra-family variance = half genetic variance)
+n.progeny <- n.replicates*n.matings
+eta.realised <- eta.family + rnorm(n.progeny, sd = sqrt(sigma2_g/2))
+
+dat <- data.frame(Id = max(sapply(parents.codes, max)) + seq.int(n.progeny),
+                  rep = rep(seq.int(n.replicates), each = n.matings),
+                  matings,
+                  eta.realised,
+                  y = eta.realised + rnorm(n.progeny, sd = sqrt(sigma2_e)))
+
+## Define variable for the non-additive SCA
+dat <- transform(dat,
+                 SCA   = factor(paste(male, female, sep = 'x'),
+                                levels = rownames(matings)))
+
+## Printing simulated setting
+print(table(dat[, c('male', 'female')]), zero.print = "")
+str(dat)
